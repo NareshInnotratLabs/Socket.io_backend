3a5e60b00bb016331357c23615e978b5b69d439b
NareshInnotratLabs <165658765+NareshInnotratLabs@users.noreply.github.com>
2024-06-07 18:23:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,27 +12,39 @@ const io = socketIo(server, {
     }
 });
 // Dummy data for demonstration
+// Dummy data for demonstration
 const videoLinks = [
+  'https://res.cloudinary.com/doy3ks7ls/video/upload/v1680075287/Intro_xb5ftf.mp4',
+  'https://res.cloudinary.com/doy3ks7ls/video/upload/v1680075215/Question_1_rwml32.mp4',
+  'https://res.cloudinary.com/doy3ks7ls/video/upload/v1680075219/Question_2_yeddjq.mp4',
+  'https://res.cloudinary.com/doy3ks7ls/video/upload/v1680075219/Question_3_kqgbgw.mp4',
+];
 
-        'https://res.cloudinary.com/dpfcfb009/video/upload/v1716547393/satya/SatyaInnoview/q20msja7ipakcirabi9r.mp4',
-      
-  ];
 let currentVideoIndex = 0;
 
 io.on('connection', (socket) => {
   console.log('Client connected');
 
-  // Send the current video link to the client
   socket.on('initialVideoRequest', () => {
-    socket.emit('videoLink', videoLinks[currentVideoIndex]);
+    if (currentVideoIndex < videoLinks.length) {
+      console.log("Sending initial video:", videoLinks[currentVideoIndex]); // Debugging log
+      socket.emit('videoLink', videoLinks[currentVideoIndex]);
+    } else {
+      console.log("No more videos to play");
+      socket.emit('videoLink', null);  // No more videos to send
+    }
   });
 
-  // Listen for next video request from the client
   socket.on('nextVideoRequest', () => {
-    currentVideoIndex = (currentVideoIndex + 1) % videoLinks.length;
-    const nextVideoLink = videoLinks[currentVideoIndex];
-    // Broadcast the new video link to all clients
-    io.emit('videoLink', nextVideoLink);
+    currentVideoIndex++;
+    if (currentVideoIndex < videoLinks.length) {
+      const nextVideoLink = videoLinks[currentVideoIndex];
+      console.log("Sending next video:", nextVideoLink); // Debugging log
+      io.emit('videoLink', nextVideoLink);
+    } else {
+      console.log("No more videos to send"); // Debugging log
+      io.emit('videoLink', null);  // No more videos to send
+    }
   });
 });
 
