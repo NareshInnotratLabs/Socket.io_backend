3ef7e8dd96ec8f1472f9ace59a646e4b4849a2fe
NareshInnotratLabs <165658765+NareshInnotratLabs@users.noreply.github.com>
2024-06-08 17:04:48 +0530

    Link Updated Done index.js

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
+
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-const path = require('path');
 const cors = require("cors");
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
-    cors: {
-      origin: "*", // Frontend URL
-      methods: ["GET", "POST"],
-    }
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  }
 });
-// Dummy data for demonstration
-// Dummy data for demonstration
-const videoLinks = [
-  'https://res.cloudinary.com/doy3ks7ls/video/upload/v1680075287/Intro_xb5ftf.mp4',
-  'https://res.cloudinary.com/doy3ks7ls/video/upload/v1680075215/Question_1_rwml32.mp4',
-  'https://res.cloudinary.com/doy3ks7ls/video/upload/v1680075219/Question_2_yeddjq.mp4',
-  'https://res.cloudinary.com/doy3ks7ls/video/upload/v1680075219/Question_3_kqgbgw.mp4',
-];
 
-let currentVideoIndex = 0;
+const videoLinks = [
+  "https://res.cloudinary.com/dpfcfb009/video/upload/v1717766249/IndividualQuestions/rrtjzajnpixain8gopmm.mp4",
+
+  "https://res.cloudinary.com/dpfcfb009/video/upload/v1717766249/IndividualQuestions/ssg89dwgfz5mthpro41p.mp4",
+
+  "https://res.cloudinary.com/dpfcfb009/video/upload/v1717766250/IndividualQuestions/cgmdquoj0gdbececwhue.mp4", 
+
+  "https://res.cloudinary.com/dpfcfb009/video/upload/v1717766250/IndividualQuestions/irjp2ofvgd97pwfxnftg.mp4",
+
+  "https://res.cloudinary.com/dpfcfb009/video/upload/v1717766249/IndividualQuestions/z7oojdt4qfg99yuwmpbr.mp4",
+];
 
 io.on('connection', (socket) => {
   console.log('Client connected');
+  
+  // Initialize video index for each client
+  let currentVideoIndex = 0;
 
   socket.on('initialVideoRequest', () => {
     if (currentVideoIndex < videoLinks.length) {
@@ -40,20 +45,20 @@ io.on('connection', (socket) => {
     if (currentVideoIndex < videoLinks.length) {
       const nextVideoLink = videoLinks[currentVideoIndex];
       console.log("Sending next video:", nextVideoLink); // Debugging log
-      io.emit('videoLink', nextVideoLink);
+      socket.emit('videoLink', nextVideoLink);
     } else {
       console.log("No more videos to send"); // Debugging log
-      io.emit('videoLink', null);  // No more videos to send
+      socket.emit('videoLink', null);  // No more videos to send
     }
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
   });
 });
 
-const PORT = process.env.PORT || 3005;
+const PORT = process.env.PORT || 3006;
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-
-
-
